abcfad032f3c7873004cc9980d4e0c437c10ab12
JoelK02 <75214236+JoelK02@users.noreply.github.com>
2023-12-26 20:33:39 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "ec2-54-209-254-134.compute-1.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lowchoonkeat-employee"
+customregion = "us-east-1"
+
